2c62426e8ebcf4404c3c72bbd395d6feab97e820
Martin Crockett <martin.crockett@gmail.com>
2012-04-08 14:14:53 +0100

    Updated page

--- README.markdown
+++ README.markdown
@@ -1,2 +1,19 @@
 README.markdown
 ==
+
+Pow
+--
+cd ~/.pow
+ln -s /Users/martin/Sites/goldenbyteconsulting-com goldenbyteconsulting-com
+cd -
+mkdir tmp
+touch tmp/restart.txt
+
+http://goldenbyteconsulting-com.dev
+
+Git
+--
+git status
+git add .
+git commit -a -m "New page"
+git commit -a -m "Updated page"
--- app.rb
+++ app.rb
@@ -9,6 +9,22 @@ get '/' do
   erb :index
 end
 
+get '/what-we-do' do
+  erb :what_we_do
+end
+
+get '/who-we-are' do
+  erb :who_we_are
+end
+
+get '/our-clients' do
+  erb :our_clients
+end
+
+get '/contact-us' do
+  erb :contact_us
+end
+
 # See http://www.sinatrarb.com/faq.html#partials
 helpers do
   def partial template
